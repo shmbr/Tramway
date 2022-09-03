735468927e6ed04bc230e7d34bac05b8c35ac01d
yu_shmbr <ura.shambora@gmail.com>
2022-09-03 17:24:44 +0300

    Basic project structure

---
+++ Tramway/SecondScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondScreenViewController.swift
+//  Tramway
+//
+//  Created by Yu_SHMBR on 03.09.2022.
+//
+
+import UIKit
+
+class SecondScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Міський транспорт"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
